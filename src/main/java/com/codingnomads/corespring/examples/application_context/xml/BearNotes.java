88c93acce02faf515ec9974819e1fc4e5c6b9b51
v605690 <v605690@gmail.com>
2025-05-08 10:51:21 -0500

    complete Learn by Doing - XML

---
+++ src/main/java/com/codingnomads/corespring/examples/application_context/xml/BearNotes.java
@@ -0,0 +1,13 @@
+package com.codingnomads.corespring.examples.application_context.xml;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+public class BearNotes {
+    private String markdown;
+    private String write;
+}
--- src/main/java/com/codingnomads/corespring/examples/application_context/xml/CodingNomad.java
+++ src/main/java/com/codingnomads/corespring/examples/application_context/xml/CodingNomad.java
@@ -11,16 +11,19 @@ public class CodingNomad {
     private final JDK jdk;
     private final IDE ide;
     private final Framework framework;
+    private final BearNotes bearNotes;
 
     public String createAwesomeSoftware() {
         return MessageFormat.format(
                 "This coding nomad is creating awesome software using, "
-                        + "IDE:({0}:{1}), JDK: ({2}:{3}), Framework:({4}:{5})",
+                        + "IDE:({0}:{1}), JDK:({2}:{3}), Framework:({4}:{5}), BearNotes:({6}:{7})",
                 ide.getName(),
                 ide.getVersion(),
                 jdk.getName(),
                 jdk.getVersion(),
                 framework.getName(),
-                framework.getVersion());
+                framework.getVersion(),
+                bearNotes.getMarkdown(),
+                bearNotes.getWrite());
     }
 }
