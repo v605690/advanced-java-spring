11d945b28a683658f2c167e78a6d9dfc5a0403d1
v605690 <v605690@gmail.com>
2025-05-18 11:17:32 -0500

    complete Learn by Doing - @Value Annotation

--- src/main/java/com/codingnomads/corespring/examples/valueannotation/Nomad.java
+++ src/main/java/com/codingnomads/corespring/examples/valueannotation/Nomad.java
@@ -36,6 +36,24 @@ public class Nomad {
     @Value("#{${database.values}}")
     private Map<String, String> databaseValues;
 
+    @Value("${bear.version}}")
+    private String bearVersion;
+
+    @Value("${bear.name}}")
+    private String bearName;
+
+    @Value("${bear.date}}")
+    private String bearDate;
+
+    @Value("${docker.version}}")
+    private String dockerVersion;
+
+    @Value("${docker.name}}")
+    private String dockerName;
+
+    @Value("${docker.engine}}")
+    private String dockerEngine;
+
     public String nomadIdentity() {
         return name.concat(" ").concat(age.toString());
     }
@@ -50,7 +68,15 @@ public class Nomad {
                 .concat(" , ")
                 .concat(framework)
                 .concat(" and ")
-                .concat(ide);
+                .concat(ide)
+                .concat(bearVersion)
+                .concat("\nNote Name & Version - ")
+                .concat(bearName)
+                .concat(bearDate)
+                .concat(dockerVersion)
+                .concat("\nContainer Name & Version - ")
+                .concat(dockerName)
+                .concat(dockerEngine);
     }
 
     public List<String> getWorkingDays() {
