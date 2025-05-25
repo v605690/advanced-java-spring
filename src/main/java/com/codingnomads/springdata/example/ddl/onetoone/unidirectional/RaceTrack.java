c35d9e73de9360067fe3f614c103d1b90a815ffd
v605690 <v605690@gmail.com>
2025-05-25 13:14:23 -0500

    complete Learn by Doing - onetoone

---
+++ src/main/java/com/codingnomads/springdata/example/ddl/onetoone/unidirectional/RaceTrack.java
@@ -0,0 +1,25 @@
+package com.codingnomads.springdata.example.ddl.onetoone.unidirectional;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "racetrack")
+@NoArgsConstructor
+@Getter
+@Setter
+public class RaceTrack {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String location;
+    private String type;
+    private String length;
+
+    @OneToOne
+    private Driver driver;
+}
