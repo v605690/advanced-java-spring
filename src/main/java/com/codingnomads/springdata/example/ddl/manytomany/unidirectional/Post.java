b412cc56c89a1367528facd10be70dcee15196f4
v605690 <v605690@gmail.com>
2025-05-26 13:04:05 -0500

    complete Learn by Doing - unidirectional

--- src/main/java/com/codingnomads/springdata/example/ddl/manytomany/unidirectional/Post.java
+++ src/main/java/com/codingnomads/springdata/example/ddl/manytomany/unidirectional/Post.java
@@ -34,4 +34,7 @@ public class Post {
     // set up many-to-many relationship with the tag class
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Set<Location> location;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Message> messages;
 }
