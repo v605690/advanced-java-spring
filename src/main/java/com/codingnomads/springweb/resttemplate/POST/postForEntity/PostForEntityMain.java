3d8cad2295566d95731faf6e25e49c90393787aa
v605690 <v605690@gmail.com>
2025-06-22 10:45:28 -0500

    complete Learn by Doing - postForEntity

--- src/main/java/com/codingnomads/springweb/resttemplate/POST/postForEntity/PostForEntityMain.java
+++ src/main/java/com/codingnomads/springweb/resttemplate/POST/postForEntity/PostForEntityMain.java
@@ -4,6 +4,9 @@ package com.codingnomads.springweb.resttemplate.POST.postForEntity;
 import com.codingnomads.springweb.resttemplate.POST.models.ResponseObject;
 import com.codingnomads.springweb.resttemplate.POST.models.Task;
 import java.util.Objects;
+
+import com.codingnomads.springweb.resttemplate.POST.models.User;
+import com.codingnomads.springweb.resttemplate.POST.models.UserResponseObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -30,18 +33,43 @@ public class PostForEntityMain {
                     .name("learn how to use postForEntity()")
                     .description("get comfortable using the RestTemplate postForEntity() method")
                     // be sure to use valid user id
-                    .userId(380)
+                    .userId(13100)
                     .completed(false)
                     .build();
 
             ResponseEntity<ResponseObject> responseEntity = restTemplate.postForEntity(
                     "http://demo.codingnomads.co:8080/tasks_api/tasks", newTask, ResponseObject.class);
 
+            User user1 = User.builder()
+                    .email("marialong@gmail.com")
+                    .firstName("Maria")
+                    .lastName("Long")
+                    .build();
+
+            User user2 = User.builder()
+                    .email("jonpaul@gmail.com")
+                    .firstName("Jon")
+                    .lastName("Paul")
+                    .build();
+
+            ResponseEntity<UserResponseObject> responseEntity2 = restTemplate.postForEntity(
+                    "http://demo.codingnomads.co:8080/tasks_api/users", user1, UserResponseObject.class);
+
+            ResponseEntity<UserResponseObject> responseEntity3 = restTemplate.postForEntity(
+                    "http://demo.codingnomads.co:8080/tasks_api/users", user2, UserResponseObject.class);
+
             if (responseEntity.getStatusCode().equals(HttpStatus.CREATED)) {
                 System.out.println(Objects.requireNonNull(responseEntity.getBody()));
-            } else {
-                System.out.println(
-                        Objects.requireNonNull(responseEntity.getBody()).getError());
+
+            } else if (responseEntity2.getStatusCode().equals(HttpStatus.CREATED)) {
+                System.out.println(Objects.requireNonNull(responseEntity2.getBody()));
+
+            } else if (responseEntity3.getStatusCode().equals(HttpStatus.CREATED)) {
+                        System.out.println(Objects.requireNonNull(responseEntity3.getBody()));
+
+                    } else {
+                        System.out.println(
+                                Objects.requireNonNull(responseEntity.getBody()).getError());
             }
         };
     }
