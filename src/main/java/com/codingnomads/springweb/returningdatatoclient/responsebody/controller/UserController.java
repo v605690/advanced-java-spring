aec2a839065cabb2c4c7002bde46daa4c82d30c2
v605690 <v605690@gmail.com>
2025-07-07 12:14:46 -0500

    complete Learn by Doing - springweb/returningdatatoclient/responsebody/controller/UserController

--- src/main/java/com/codingnomads/springweb/returningdatatoclient/responsebody/controller/UserController.java
+++ src/main/java/com/codingnomads/springweb/returningdatatoclient/responsebody/controller/UserController.java
@@ -7,14 +7,42 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
+
+     List<User> userList;
+
+    public UserController() {
+        userList = new ArrayList<>();
+    }
+
 
     public User user = User.builder()
             .id(1000)
             .name("Spring Dev")
             .email("dev@codingnomads.com")
+            .build();
+
+    public User user2 = User.builder()
+            .id(1002)
+            .name("Java 101")
+            .email("java101@codingnomads.com")
+            .build();
+
+    public User user3 = User.builder()
+            .id(1003)
+            .name("Java 201")
+            .email("java201@codingnomads.com")
+            .build();
+    public User user4 = User.builder()
+            .id(1004)
+            .name("Java 301")
+            .email("java301@codingnomads.com")
             .build();
 
     // using ResponseBody to return a POJO
@@ -34,5 +62,16 @@ public class UserController {
     @GetMapping("/user")
     public User user() {
         return user;
+    }
+
+    @ResponseBody
+    @GetMapping("/user-list")
+    public <userList> userList fetchUserList() {
+       userList.add(user);
+       userList.add(user2);
+       userList.add(user3);
+       userList.add(user4);
+
+       return (userList) userList;
     }
 }
