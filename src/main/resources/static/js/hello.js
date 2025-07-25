93bfb9e7c747f6bc3faf7f9f733e420a8e365338
v605690 <v605690@gmail.com>
2025-07-25 11:50:36 -0500

    update and complete main.css, hello.js, practice.html, thymeleaf-include-js-css.html

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -2,3 +2,27 @@ function greeting() {
     alert("Hello Spring Developer!!");
 }
 
+function greet() {
+    alert("Greetings Pravin");
+}
+
+function thing() {
+    let str1
+    str1 =  "A Full Stack Developer";
+}
+
+function myinput(){
+    var name = prompt("Enter Your Name:");
+
+    if (name == null || name === "") {
+        document.getElementById("msg").innerHTML = "You did not enter anything. Please enter your name again";
+    }
+    else
+    {
+        document.getElementById("msg").innerHTML = "You entered: " + name;
+    }
+}
+
+
+
+
