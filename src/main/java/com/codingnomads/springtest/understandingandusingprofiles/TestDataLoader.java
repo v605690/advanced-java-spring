8f13b201bcc1cc9a55fbd0e98f1f9675a2eb2f8e
v605690 <v605690@gmail.com>
2025-08-07 10:32:09 -0500

    complete spring test - TestDataLoader, application.properties

--- src/main/java/com/codingnomads/springtest/understandingandusingprofiles/TestDataLoader.java
+++ src/main/java/com/codingnomads/springtest/understandingandusingprofiles/TestDataLoader.java
@@ -3,6 +3,8 @@ package com.codingnomads.springtest.understandingandusingprofiles;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +14,17 @@ public class TestDataLoader implements CommandLineRunner {
 
     @Autowired
     CoffeePreferenceRepo repo;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    @Profile("test2")
+    public String coffee() {
+        System.out.println("Americano now created");
+        System.out.println("Espresso now created");
+        return "Coffee beans initialized";
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -31,5 +44,12 @@ public class TestDataLoader implements CommandLineRunner {
                 .intensity(4)
                 .sugar(true)
                 .build());
+
+        try {
+            String coffeeBean = applicationContext.getBean("coffee", String.class);
+            System.out.println("Coffee status: " + coffeeBean);
+        } catch (Exception e) {
+            System.out.println("Coffee not available (profile not active)");
+        }
     }
 }
