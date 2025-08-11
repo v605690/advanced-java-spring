1542c072d46007bce6c4095c4ed0ecb8b59d9848
v605690 <v605690@gmail.com>
2025-08-11 14:59:04 -0500

    Spring Test Lab - add and update springtest/lab/MovieControllerTest

--- src/test/java/com/codingnomads/springtest/lab/MovieControllerTest.java
+++ src/test/java/com/codingnomads/springtest/lab/MovieControllerTest.java
@@ -1,11 +1,19 @@
 /* CodingNomads (C)2024 */
 package com.codingnomads.springtest.lab;
 
+import com.codingnomads.springtest.lab.entity.Movie;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest(classes = SpringTestLab.class)
 @AutoConfigureMockMvc
@@ -15,10 +23,27 @@ public class MovieControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void testGetAllMoviesSuccess() {}
+    public void testGetAllMoviesSuccess() throws Exception {
+
+        mockMvc
+                .perform(get("/all"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("The Shawshank Redemption"))
+                .andExpect(jsonPath("$[0].rating").value(9.3))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("The Pursuit of Happyness"))
+                .andExpect(jsonPath("$[1].rating").value(8.0));
+    }
 
     @Test
-    public void testGetAllMoviesFailure() {}
+    public void testGetAllMoviesFailure() throws Exception {
+        mockMvc.perform(get("/all/3"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
 
     @Test
     public void testGetAllMoviesSuccessMockService() {}
